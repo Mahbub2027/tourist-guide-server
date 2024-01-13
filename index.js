5f7b9710c635eccf1492b957e003dbfcae7506f7
Mahbub2027 <alam201127@gmail.com>
2024-01-14 00:41:36 +0600

    bookings collection added

--- index.js
+++ index.js
@@ -28,8 +28,28 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const userCollection = client.db('touristDB').collection('users');
         const packageCollection = client.db('touristDB').collection('packages');
         const storyCollection = client.db('touristDB').collection('stories');
+        const bookingCollection = client.db('touristDB').collection('bookings');
+
+        // users collection
+        app.post('/users', async(req, res)=>{
+            const user= req.body;
+            // if users exits or not
+            const query = {email : user.email}
+            const exitingUser = await userCollection.findOne(query)
+            if(exitingUser){
+                return res.send({message: 'user already exits', insertedId: null})
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/users', async(req, res)=>{
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
 
         // packages collection
         app.get('/packages', async(req, res)=>{
@@ -59,6 +79,35 @@ async function run() {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
             const result = await storyCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/stories', async(req, res)=>{
+            const story = req.body;
+            const result = await storyCollection.insertOne(story);
+            res.send(result);
+        })
+
+        // booking collection
+        app.post('/bookings', async(req, res)=>{
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.get('/bookings', async(req, res)=>{
+            let query = {};
+            if(req.query?.email){
+                query = {email: req.query.email}
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await bookingCollection.deleteOne(query);
             res.send(result);
         })
 
